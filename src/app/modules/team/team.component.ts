6caf07eea44c9b045661b39ca2ac38cf6a838c32
Jair Martin del Campo <jairantonio21@hotmail.com>
2024-06-06 22:51:25 -0600

    se pueden ordenar los datos de los jugadores en la tabla

--- src/app/modules/team/team.component.ts
+++ src/app/modules/team/team.component.ts
@@ -11,16 +11,26 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { DeletePlayerComponent } from './components/delete-player/delete-player.component';
 import { EditPlayerComponent } from './components/edit-player/edit-player.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-team',
   standalone: true,
   templateUrl: './team.component.html',
   styleUrl: './team.component.css',
-  imports: [NavbarComponent, CommonModule, MatMenuModule, MatIconModule],
+  imports: [NavbarComponent, CommonModule, MatMenuModule, MatIconModule, FormsModule],
 })
 export class TeamComponent {
+  
   players: ResultPlayers[] = [];
+  filteredPlayers: ResultPlayers[] = [...this.players];
+
+
+  sortColumn = 'nombre'; 
+  sortOrder = 'asc'; 
+
+  searchTerm: string = '';
+
 
   constructor(
     public dialog: MatDialog,
@@ -31,6 +41,44 @@ export class TeamComponent {
   ngOnInit(): void {
     this.getPlayers();
   }
+
+  sortData(column: keyof ResultPlayers) {
+    if (this.sortColumn === column) {
+      this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortColumn = column;
+      this.sortOrder = 'asc';
+    }
+  
+    this.players.sort((a, b) => {
+      let valueA = a[column];
+      let valueB = b[column];
+  
+      valueA = (valueA ?? "").toString();
+      valueB = (valueB ?? "").toString();
+  
+      // Comparación especial para el dorsal ya que es un string pero se debe comparar como número
+      if (column === 'dorsal') {
+        const numA = parseInt(valueA, 10);
+        const numB = parseInt(valueB, 10);
+        if (!isNaN(numA) && !isNaN(numB)) {
+          return this.sortOrder === 'asc' ? numA - numB : numB - numA;
+        }
+      }
+      // Comparación general como strings
+      valueA = valueA.toLowerCase();
+      valueB = valueB.toLowerCase();
+      if (valueA < valueB) {
+        return this.sortOrder === 'asc' ? -1 : 1;
+      }
+      if (valueA > valueB) {
+        return this.sortOrder === 'asc' ? 1 : -1;
+      }
+      return 0;
+    });
+  }
+  
+  
 
   getPlayers() {
     const teamId = Number(localStorage.getItem(LOCAL_STORAGE.TeamId));
